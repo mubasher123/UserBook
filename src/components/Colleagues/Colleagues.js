faf1141d3c6e9f54afb77474146ba11d04a1b182
Mubasher Ahmad <mubasherahmad2@gmail.com>
2020-06-03 16:10:10 +0500

    commented code removed

--- src/components/Colleagues/Colleagues.js
+++ src/components/Colleagues/Colleagues.js
@@ -1,73 +1,7 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { Table} from 'antd';
+import { Table } from 'antd';
 
-// import Panel from '../Panel/Panel';
-
-// import './Colleagues.css';
-
-// const Colleague = ({ photo, name, nationality, email, country}) => (
-//   <tr className="colleague">
-//     <td>
-//       <img className="colleague__photo" src={photo} alt={name} />
-//     </td>
-//     <td className="colleague__info">
-//       <div className="colleague__name">{name}</div>
-//       <div className="colleague__status">{nationality}</div>
-//     </td>
-//     <td className="colleague__info">
-//       <div className="colleague__name">{email}</div>
-//     </td>
-//     <td className="colleague__info">
-//       <div className="colleague__name">{country}</div>
-//     </td>
-//   </tr>
-// );
-
-// class Colleagues extends Component {
-//   render() {
-//     let panelContent;
-
-//     if (this.props.loading) {
-//       panelContent = (
-//         <p>Loading...</p>
-//       );
-//     } else {
-//       if (this.props.error) {
-//         panelContent = (
-//           <p>Something went wrong while fetching the data: <code>{this.props.error}</code></p>
-//         );
-//       } else {
-//         panelContent = (
-//           <table className="colleagues-table">
-//             <colgroup>
-//               <col width="0" />
-//               <col width="100%" />
-//             </colgroup>
-//             <tbody>
-//             {this.props.usersList.map((person, index) => (
-//               <Colleague
-//                 key={index}
-//                 photo={person.picture.thumbnail}
-//                 name={person.name.title +'. '+ person.name.first + ' ' + person.name.last}
-//                 nationality={person.nat}
-//                 email={person.email}
-//                 country={person.location.country}
-//               />
-//             ))}
-//             </tbody>
-//           </table>
-//         );
-//       }
-//     }
-
-//     return (
-//       <Panel title="Users List">
-//         {panelContent}
-//       </Panel>
-//     );
-//   }
-// }
 const columns = [
   {
     title: 'Name',
@@ -98,6 +32,7 @@ const columns = [
     render: text => {if(text === 'male'){text = 'M'}else{text = 'F'} return text},
   },
 ];
+
 class Colleagues extends Component {
   render() {
     return (
