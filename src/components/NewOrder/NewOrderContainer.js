08f612927703c275ba826f19ba986cd1398e917a
Mubasher Ahmad <mubasherahmad2@gmail.com>
2020-06-03 16:14:04 +0500

    un used files deleted

--- src/components/NewOrder/NewOrderContainer.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import NewOrder from './NewOrder';
-import { actions as ordersActions } from '../../ducks/orders';
-
-const mapDispatchToProps = (dispatch) => ({
-  placeOrder: (orderData) => dispatch(ordersActions.placeOrder(orderData)),
-});
-
-export default connect(null, mapDispatchToProps)(NewOrder);
